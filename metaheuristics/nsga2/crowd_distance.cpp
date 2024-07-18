13ac8c924e42d3359e392710d91dff5b3a8ebc5f
João Gabriel <jg.lofiego@gmail.com>
2024-07-18 13:11:21 -0300

    adiciona comentarios e troca o nome da funcao

--- metaheuristics/nsga2/crowd_distance.cpp
+++ metaheuristics/nsga2/crowd_distance.cpp
@@ -13,49 +13,64 @@ bool cmp_cd(pair<int, double>& a, pair<int, double>&b){
     return a.second >= b.second;
 }
 
-vector<Solution> sort_distance(vector<Solution> &population){
+vector<Solution> crowding_distance(vector<Solution> &population){
+    //Constantes Reutilizáveis
     int num = population.size();
     pair<int, double> dfault = make_pair(0, 1);
     float infinity = numeric_limits<float>::max();
+    
+    //Inicializa um vetor de índices que vai servir como base para vários vetores
+    vector<pair<int, double>> base(num, dfault);
+    for(int i = 0; i < num; i++){
+        base[i].first = i;
+    }
 
-    vector<pair<int, double>> byF1(num, dfault), byF2(num, dfault), final(num, dfault);
+    //Inicializa 3 vetores de pair(int, double)
+    //byf1 é o vetor que armazena o índice no first e o valor na função 1 no second
+    //byf2 é o vetor que armazena os índices no first e o valor na função 2 no second
+    //final é o vetor que armazena os índices no first e o valor do crowding distance no second
+    //aux é uma cópia da população para auxiliar a rearranjar o vetor
+    vector<pair<int, double>> byF1, byF2, final;
+    byF1 = byF2 = final = base;
     vector<Solution> aux = population;
 
+    //loop que a cada iteração coloca o valor do second como sendo o valor da respectiva função
     for(int i=0; i<num; i++){
-        byF1[i].first = i;
-        byF2[i].first = i;
-        final[i].first = i;
         byF1[i].second = population[i].fitness.first;
         byF2[i].second = population[i].fitness.second;
     }
 
+    //Ordena os vetores baseados no second
     sort(byF1.begin(), byF1.end(), cmp_second);
     sort(byF2.begin(), byF2.end(), cmp_second);
 
-    vector<pair<int, double>> distancesF1(num, dfault), distancesF2(num, dfault);
+    //Inicializa outros dois vetores de pares para armazenar os resultados do crowding distance de cada uma das funções 
+    vector<pair<int, double>> distancesF1, distancesF2;
+    distancesF1 = byF1;
+    distancesF2 = byF2;
 
-    for(int i = 0; i < num; i++){
-        distancesF1[i].first = byF1[i].first;
-        distancesF2[i].first = byF2[i].first;
-    }
-
+    //Atribui as pontas um valor muito grande
     distancesF1[0].second = infinity;
     distancesF2[0].second = infinity;
     distancesF1[num-1].second = infinity;
     distancesF2[num-1].second = infinity;
 
+    //Atribui no second o valor do Crowding distance daquela solução em relação aos vizinhos
     for(int i = 1; i < num-1; i++){
         distancesF1[i].second = population[byF1[i+1].first].fitness.first - population[byF1[i-1].first].fitness.first;
         distancesF2[i].second = population[byF2[i+1].first].fitness.second - population[byF2[i-1].first].fitness.second;
     }
 
+    //Coloca no vetor final o valor da multiplicação do crowding de f1 com o crowding de f2
     for(int i = 0; i < num; i++) {
         final[distancesF1[i].first].second *= distancesF1[i].second;
         final[distancesF2[i].first].second *= distancesF2[i].second;
     }
 
+    //Ordena o vetor de forma decrescente em relação ao second
     sort(final.begin(), final.end(), cmp_cd);
 
+    //Pega o o valor do índice correto na ordenação e copia para os elementos de population
     for(int i = 0; i < num; i++) {
         population[i] = aux[final[i].first];
     }
@@ -63,6 +78,7 @@ vector<Solution> sort_distance(vector<Solution> &population){
     return population;
 }
 
+// Teste do algorítmo
 int main(){
     vector<Solution> v;
     vector<int> grid;
@@ -111,7 +127,7 @@ int main(){
     v.push_back(sol4);
     v.push_back(sol5);
 
-    sort_distance(v);
+    crowding_distance(v);
 
     cout << "RESULTADO FINAL:\n";
     for(int i = 0; i < v.size(); i++){
