f5f8d692ecb4c6ff1b7198c865265eb4dc7232be
gustavojorge <gustavojorge080@gmail.com>
2025-01-09 02:00:19 -0300

    Update v4

--- headers/adaptative_grid/Grid.h
+++ headers/adaptative_grid/Grid.h
@@ -10,23 +10,17 @@ private:
     int grid[1 << (NUMOBJETIVOS * PROFUNDIDADEGRID)];
 
 public:
-    // Construtor
-    Grid();  // Declaração do construtor, sem a implementação
+    Grid();  
 
-    // Retorna a contagem de posições no grid
     int getPositionCount(int p);
 
-    // Adiciona uma posição no grid
     void addGrid(int p);
 
-    // Remove uma posição do grid
     void removeGrid(int p);
 
-    // Limpa o grid
     void clearGrid();
 
-    // Retorna o tamanho do grid
     int getSize();
 };
 
-#endif // GRID_H
+#endif
